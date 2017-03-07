f73ff8eaa5965a5e568ea8cad6c707888112cd67
Fabio Kawasaki <binhu_kawa@hotmail.com>
2017-03-07 11:16:17 -0300

    Exercicios

---
+++ pratica1/exercicio2/MeuPrograma.java
@@ -0,0 +1,8 @@
+// Fabio Kawasaki 13.02838-3
+public class MeuPrograma {
+	public static void main(String[] args) {
+		
+		// Mensagem de saida do sistema
+		System.out.println("Minha primeira aplicação Java!!");
+	}
+}
