b222bec2cdef949e4f6f43a1b00e5a98b091b6d1
Fabio Kawasaki <binhu_kawa@hotmail.com>
2017-03-08 11:26:33 -0300

    Aula 03

--- pratica1/exercicio4/Aula03/src/aula03/Aula03.java
+++ pratica1/exercicio4/Aula03/src/aula03/Aula03.java
@@ -5,10 +5,8 @@ public class Aula03 {
     public static void main(String[] args) {
         Conta c1 = new Conta();             
         c1.saldo = 1000;
-        c1.visualizarSaldo();
-        
-        c1.sacar(200);
-        
+        c1.visualizarSaldo();        
+        c1.sacar(200);        
         c1.visualizarSaldo();
         
         Conta c2 = new Conta();             
